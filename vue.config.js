52106ac974cafa016dbcbaa7c0dd809ea5fb48b6
wangbao <2363745556@qq.com>
2024-03-29 16:43:22 +0800

    20240329 - 增加aps系统功能

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 'use strict'
 const path = require('path')
+const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 function resolve(dir) {
   return path.join(__dirname, dir)
@@ -69,6 +70,16 @@ module.exports = {
         filename: '[path].gz[query]',   // 压缩后的文件名
         algorithm: 'gzip',              // 使用gzip压缩
         minRatio: 0.8                   // 压缩率小于1才会压缩
+      }),
+      new FileManagerPlugin({
+        events: {
+          onEnd: {
+            delete: [`./dist/*.zip`],
+            archive: [
+              {source: `./dist`, destination: `./dist/app.zip`}
+            ]
+          }
+        }
       })
     ],
   },
