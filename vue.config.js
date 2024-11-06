f7e5d7d5ab5539d59c5a149337fd97081c3cf9e0
wangbao <197235339@qq.com>
2024-11-06 18:15:38 +0800

    feat （报表）:  增加部分报表 （demo）

--- vue.config.js
+++ vue.config.js
@@ -36,8 +36,8 @@ module.exports = {
     // https: true,
     proxy: {
       '/api': {
-        target: 'http://localhost',//代理地址，这里设置的地址会代替axios中设置的baseURL
-        // target: 'https://aps.solveplan.cn/',//代理地址，这里设置的地址会代替axios中设置的baseURL
+        // target: 'http://localhost',//代理地址，这里设置的地址会代替axios中设置的baseURL
+        target: 'https://aps.solveplan.cn/',//代理地址，这里设置的地址会代替axios中设置的baseURL
         changeOrigin: true,// 如果接口跨域，需要进行这个参数配置
         //ws: true, // proxy websockets
         //pathRewrite方法重写url
