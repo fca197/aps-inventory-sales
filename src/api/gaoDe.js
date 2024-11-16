c5a71f73d616762ad4ba7d62723693e0116597c4
wangbao <197235339@qq.com>
2024-11-16 18:42:01 +0800

    feat （销售门店）:  增加

--- src/api/gaoDe.js
+++ src/api/gaoDe.js
@@ -12,11 +12,78 @@ export function addGdMap(elId) {
 
 export function getLocationByLanLon(lat, lon, callBack) {
   //restapi.amap.com/v3/geocode/regeo?key=您的key&location=116.481488,39.990464&poitype=&radius=&extensions=base&roadlevel=1
-  post('/gd/restapi/geocode/regeo/' + lon + '%2C' + lat,{},false).then(r => {
+  post('/gd/restapi/geocode/regeo/' + lon + '%2C' + lat, {}, false).then(r => {
     let regeocode = r.regeocode
     log(lat, lon, regeocode)
     if (callBack) {
       callBack(regeocode)
     }
   })
+}
+
+export function initGdMap(_t,gdMapId ,func) {
+  _t.$nextTick(() => {
+    // 子组件加载成功后执行的代码
+    // console.log('Child component has been mounted');
+    log(document.getElementById(gdMapId))
+    _t.gdMap = new AMap.Map('sellerStoreAddrInputDiv', {
+      resizeEnable: true,
+      center: [116.30946, 39.937629],
+      zoom: 13
+    })
+
+    AMap.plugin(['AMap.PlaceSearch', 'AMap.DistrictSearch'], function() {
+      _t.districtSearch = new AMap.DistrictSearch({
+        // 关键字对应的行政区级别，country表示国家
+        level: 'city',
+        //  显示下级行政区级数，1表示返回下一级行政区
+        subdistrict: 1
+      })
+      _t.placeSearch = new AMap.PlaceSearch({
+        map: _t.gdMap
+      })  //构造地点查询类
+      _t.placeSearch.on('markerClick', markerClick)//注册监听，当选中某条记录时会触发
+      _t.gdMap.on('click', function(e) {
+        if (_t.mapMark) {
+          _t.mapMark.setMap(null)
+          _t.mapMark = null
+        }
+        log(e)
+        // _t.form.sellerStoreAddr=e.lnglat.address
+        _t.form.sellerStoreGdLon = e.lnglat.lng
+        _t.form.sellerStoreGdLat = e.lnglat.lat
+
+        _t.mapMark = new AMap.Marker({
+          icon: '//a.amap.com/jsapi_demos/static/demo-center/icons/poi-marker-default.png',
+          position: [e.lnglat.lng, e.lnglat.lat],
+          offset: new AMap.Pixel(-13, -30)
+        })
+        _t.mapMark.setMap(_t.gdMap)
+        getLocationByLanLon(e.lnglat.lat, e.lnglat.lng, (r) => {
+          _t.form.sellerStoreAreaCode = r.addressComponent.adcode
+          _t.form.sellerStoreAddr = r.formatted_address.replace(
+            r.addressComponent.province, '').replace(
+            r.addressComponent.district, '')
+        })
+      })
+
+      function markerClick(params) {
+        log(params)
+        _t.form.sellerStoreAddr = '(' + params.data.adname + ')'
+          + params.data.address
+        _t.form.sellerStoreGdLon = params.data.location.lng
+        _t.form.sellerStoreGdLat = params.data.location.lat
+        _t.form.sellerStoreAreaCode = params.data.adcode
+        log(_t.form)
+        if (_t.mapMark) {
+          _t.mapMark.setMap(null)
+          _t.mapMark = null
+        }
+      }
+      if (func){
+        func()
+      }
+    })
+  })
+
 }
--- src/views/system/aps/apsSellerStore/ApsSellerStore.vue
+++ src/views/system/aps/apsSellerStore/ApsSellerStore.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app-container">
     <el-form v-show="showSearch" ref="queryForm" :inline="true" :model="queryParams" label-width="128px" size="small">
-      <el-form-item label="销售门店" prop="sellerStoreCode">
+      <el-form-item label="销售门店编码" prop="sellerStoreCode">
         <el-input v-model="queryParams.data.sellerStoreCode" clearable placeholder="请输入销售门店"
                   @keyup.enter.native="handleQuery"
         />
@@ -17,18 +17,18 @@
         />
       </el-form-item>
       <el-form-item label="销售门店省" prop="sellerStoreProvinceCode">
-        <el-select clearable filterable v-model="queryParams.data.sellerStoreProvinceCode">
-          <el-option v-for="(p,i) in provinceCodeList" :key="p.code" :label="p.name" :value="p.code"></el-option>
+        <el-select clearable filterable v-model="queryParams.data.sellerStoreProvinceCode" @change="value=>changeRegCode(value,'p')">
+          <el-option v-for="(p,i) in provinceCodeList" :label="p.name" :value="p.code"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="销售门店市" prop="sellerStoreCityCode">
-        <el-select clearable filterable v-model="queryParams.data.sellerStoreCityCode">
-          <el-option v-for="(p,i) in cityCodeList" :key="p.code" :label="p.name" :value="p.code"></el-option>
+        <el-select clearable filterable v-model="queryParams.data.sellerStoreCityCode" @change="value=>changeRegCode(value,'c')" >
+          <el-option v-for="(p,i) in cityCodeList" :label="p.name" :value="p.code"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="销售门店区" prop="sellerStoreAreaCode">
-        <el-select clearable filterable v-model="queryParams.data.sellerStoreAreaCode">
-          <el-option v-for="(p,i) in areaCodeList" :key="p.code" :label="p.name" :value="p.code"></el-option>
+        <el-select clearable filterable v-model="queryParams.data.sellerStoreAreaCode"  @change="value=>changeRegCode(value,'a')" >
+          <el-option v-for="(p,i) in areaCodeList" :label="p.name" :value="p.code"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="销售门店地址" prop="sellerStoreAddr">
@@ -78,7 +78,7 @@
     <el-dialog :title="title" :visible.sync="open" append-to-body width="700px">
       <el-form ref="form" :model="form" :rules="rules" label-width="140px">
 
-        <el-form-item label="销售门店" prop="sellerStoreCode">
+        <el-form-item label="销售门店编码" prop="sellerStoreCode">
           <el-input v-model="form.sellerStoreCode" clearable placeholder="请输入销售门店"/>
         </el-form-item>
         <el-form-item label="销售门店名称" prop="sellerStoreName">
@@ -89,17 +89,17 @@
         </el-form-item>
         <el-form-item label="销售门店省" prop="sellerStoreProvinceCode">
           <el-select clearable filterable v-model="form.sellerStoreProvinceCode">
-            <el-option v-for="(p,i) in provinceCodeList" :key="p.code" :label="p.name" :value="p.code"></el-option>
+            <el-option v-for="(p,i) in provinceCodeList" :label="p.name" :value="p.code"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="销售门店市" prop="sellerStoreCityCode">
           <el-select clearable filterable v-model="form.sellerStoreCityCode">
-            <el-option v-for="(p,i) in cityCodeList" :key="p.code" :label="p.name" :value="p.code"></el-option>
+            <el-option v-for="(p,i) in cityCodeList" :label="p.name" :value="p.code"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="销售门店区" prop="sellerStoreAreaCode">
           <el-select clearable filterable v-model="form.sellerStoreAreaCode">
-            <el-option v-for="(p,i) in areaCodeList" :key="p.code" :label="p.name" :value="p.code"></el-option>
+            <el-option v-for="(p,i) in areaCodeList" :label="p.name" :value="p.code"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="位置筛选">
@@ -123,7 +123,7 @@
 <script>
 
 import { add, deleteByIdList, getById, getDistrictByParentCode, log, queryPageList, updateById } from '@/api/common'
-import { addGdMap, getLocationByLanLon } from '@/api/gaoDe'
+import { addGdMap, getLocationByLanLon, initGdMap } from '@/api/gaoDe'
 
 export default {
   name: 'tenantName',
@@ -150,6 +150,7 @@ export default {
       title: '',
       // 是否显示弹出层
       open: false,
+      loadDistrict: true,
       // 查询参数
       queryParams: {
         pageNum: 1,
@@ -180,33 +181,7 @@ export default {
     }
   },
   watch: {
-    'form.sellerStoreProvinceCode'(n, o) {
-      this.getDistrict(n, this.cityCodeList)
-      this.form.sellerStoreCityCode = ''
-      this.form.sellerStoreAreaCode = ''
-      this.resetMapCenter('province', n)
-    },
-    'form.sellerStoreCityCode'(n, o) {
-      this.getDistrict(n, this.areaCodeList)
-      this.form.sellerStoreAreaCode = ''
-      // log("tm",n)
-      this.resetMapCenter('city', n)
-    },
-    'form.sellerStoreAreaCode'(n, o) {
-      if (n !== undefined && n !== '') {
-        return
-      }
-      this.resetMapCenter('district', n)
-    },
-    'queryParams.data.sellerStoreProvinceCode'(n, o) {
-      this.getDistrict(n, this.cityCodeList)
-      this.form.sellerStoreCityCode = ''
-      this.form.sellerStoreAreaCode = ''
-    },
-    'queryParams.data.sellerStoreCityCode'(n, o) {
-      this.getDistrict(n, this.areaCodeList)
-      this.form.sellerStoreAreaCode = ''
-    },
+
     'form.sellerStoreAddrInput'(n, o) {
       log(this.placeSearch)
       if (this.placeSearch) {
@@ -243,6 +218,21 @@ export default {
     cancel() {
       this.open = false
       this.reset()
+    },
+    changeRegCode(n, type) {
+      if ('p' === type) {
+        this.getDistrict(n, this.cityCodeList)
+        this.form.sellerStoreCityCode = ''
+        this.form.sellerStoreAreaCode = ''
+        this.resetMapCenter('province', n)
+      } else if ('c' === type) {
+        this.getDistrict(n, this.areaCodeList)
+        this.form.sellerStoreAreaCode = ''
+        // log("tm",n)
+        this.resetMapCenter('city', n)
+      } else if ('a' === type) {
+        this.resetMapCenter('district', n)
+      }
     },
     // 表单重置
     reset() {
@@ -282,80 +272,37 @@ export default {
       this.reset()
       this.title = '添加销售门店'
       this.open = true
-
-      let _t = this
-      this.$nextTick(() => {
-        // 子组件加载成功后执行的代码
-        // console.log('Child component has been mounted');
-        log(document.getElementById('sellerStoreAddrInputDiv'))
-        _t.gdMap = new AMap.Map('sellerStoreAddrInputDiv', {
-          resizeEnable: true,
-          center: [116.30946, 39.937629],
-          zoom: 10
-        })
-
-        AMap.plugin(['AMap.PlaceSearch', 'AMap.DistrictSearch'], function() {
-          _t.districtSearch = new AMap.DistrictSearch({
-            // 关键字对应的行政区级别，country表示国家
-            level: 'city',
-            //  显示下级行政区级数，1表示返回下一级行政区
-            subdistrict: 1
-          })
-          _t.districtSearch.search('中国', function(status, result) {
-            // 查询成功时，result即为对应的行政区信息
-            // log('districtSearch',status,result)
-
-          })
-          _t.placeSearch = new AMap.PlaceSearch({
-            map: _t.gdMap
-          })  //构造地点查询类
-          _t.placeSearch.on('markerClick', markerClick)//注册监听，当选中某条记录时会触发
-          _t.gdMap.on('click', function(e) {
-            if (_t.mapMark) {
-              _t.mapMark.setMap(null)
-              _t.mapMark = null
-            }
-            log(e)
-            // _t.form.sellerStoreAddr=e.lnglat.address
-            _t.form.sellerStoreGdLon = e.lnglat.lng
-            _t.form.sellerStoreGdLat = e.lnglat.lat
-
-            _t.mapMark = new AMap.Marker({
-              icon: '//a.amap.com/jsapi_demos/static/demo-center/icons/poi-marker-default.png',
-              position: [e.lnglat.lng, e.lnglat.lat],
-              offset: new AMap.Pixel(-13, -30)
-            })
-            _t.mapMark.setMap(_t.gdMap)
-            getLocationByLanLon(e.lnglat.lat, e.lnglat.lng, (r) => {
-              _t.form.sellerStoreAreaCode = r.addressComponent.adcode
-              _t.form.sellerStoreAddr = r.formatted_address.replace(r.addressComponent.province, '').replace(r.addressComponent.district, '')
-            })
-          })
-
-          function markerClick(params) {
-            log(params)
-            _t.form.sellerStoreAddr = '(' + params.data.adname + ')' + params.data.address
-            _t.form.sellerStoreGdLon = params.data.location.lng
-            _t.form.sellerStoreGdLat = params.data.location.lat
-            _t.form.sellerStoreAreaCode = params.data.adcode
-            log(_t.form)
-            if (_t.mapMark) {
-              _t.mapMark.setMap(null)
-              _t.mapMark = null
-            }
-          }
-        })
-      })
+      this.loadGdMap()
 
     },
     /** 修改按钮操作 */
     handleUpdate(row) {
       this.reset()
       let req = { idList: [row.id], pageSize: 1, pageNum: 1 }
+      let _t = this
+      _t.loadDistrict = false
       getById(req).then(response => {
-        this.form = response.data.dataList[0]
-        this.title = '修改销售门店'
-        this.open = true
+        let data = response.data.dataList[0]
+        Promise.all([_t.getDistrict(data.sellerStoreProvinceCode, _t.cityCodeList),
+          _t.getDistrict(data.sellerStoreCityCode, _t.areaCodeList)]
+        )
+        .then(tt => {
+          _t.open = true
+          _t.form = data
+          _t.$nextTick(() => {
+            let gdMapId='sellerStoreAddrInputDiv';
+            initGdMap(_t,gdMapId ,()=>{
+              _t.gdMap.setCenter([parseFloat(data.sellerStoreGdLon),parseFloat(data.sellerStoreGdLat)])
+              _t.mapMark = new AMap.Marker({
+                icon: '//a.amap.com/jsapi_demos/static/demo-center/icons/poi-marker-default.png',
+                position: [parseFloat(data.sellerStoreGdLon),parseFloat(data.sellerStoreGdLat)],
+                offset: new AMap.Pixel(-13, -30)
+              })
+              _t.mapMark.setMap(_t.gdMap)
+            });
+            _t.loadDistrict = true
+          })
+        })
       })
     },
 
@@ -396,7 +343,7 @@ export default {
 
     getDistrict(parentCode, list) {
       log('getDistrict ', parentCode, list)
-      if (parentCode === '') {
+      if (parentCode === '' || parentCode === undefined) {
         list.splice(0, list.length)
         return Promise.reject()
       }
@@ -416,6 +363,11 @@ export default {
           _t.gdMap.setCenter(result.districtList[0].center)
         })
       }
+    },
+    loadGdMap() {
+      let _t = this
+      let gdMapId='sellerStoreAddrInputDiv';
+      initGdMap(_t,gdMapId );
     }
   }
 
