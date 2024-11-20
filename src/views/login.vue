ed542d21cf2650ef3e16ac9b9dfdb95e4fa7a6ba
wangbao <197235339@qq.com>
2024-11-20 18:01:46 +0800

    feat （login）:  增加其他登录方式

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,60 +1,70 @@
 <template>
   <div class="login">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
-      <h3 class="title">后台管理系统</h3>
-      <el-form-item prop="username">
-        <el-input
+    <div v-if="loginType===2" class="dingLoginDiv">
+      <div class="usePwdLogin" @click="loginType=1"></div>
+      <h3 class="title"> 后台管理系统</h3>
+
+      <div>
+      </div>
+    </div>
+    <div v-if="loginType===1">
+      <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
+        <div class="useOtherLogin" @click="loginType=2"></div>
+        <h3 class="title"> 后台管理系统</h3>
+        <el-form-item prop="username">
+          <el-input
             v-model="loginForm.username"
             auto-complete="off"
             placeholder="账号"
             type="text"
-        >
-          <svg-icon slot="prefix" class="el-input__icon input-icon" icon-class="user"/>
-        </el-input>
-      </el-form-item>
-      <el-form-item prop="password">
-        <el-input
+          >
+            <svg-icon slot="prefix" class="el-input__icon input-icon" icon-class="user"/>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
             v-model="loginForm.password"
             auto-complete="off"
             placeholder="密码"
             type="password"
             @keyup.enter.native="handleLogin"
-        >
-          <svg-icon slot="prefix" class="el-input__icon input-icon" icon-class="password"/>
-        </el-input>
-      </el-form-item>
-      <el-form-item v-if="captchaEnabled" prop="code">
-        <el-input
+          >
+            <svg-icon slot="prefix" class="el-input__icon input-icon" icon-class="password"/>
+          </el-input>
+        </el-form-item>
+        <el-form-item v-if="captchaEnabled" prop="code">
+          <el-input
             v-model="loginForm.code"
             auto-complete="off"
             placeholder="验证码"
             style="width: 63%"
             @keyup.enter.native="handleLogin"
-        >
-          <svg-icon slot="prefix" class="el-input__icon input-icon" icon-class="validCode"/>
-        </el-input>
-        <div class="login-code">
-          <img :src="codeUrl" class="login-code-img" @click="getCode"/>
-        </div>
-      </el-form-item>
-      <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
-      <el-form-item style="width:100%;">
-        <el-button
+          >
+            <svg-icon slot="prefix" class="el-input__icon input-icon" icon-class="validCode"/>
+          </el-input>
+          <div class="login-code">
+            <img :src="codeUrl" class="login-code-img" @click="getCode"/>
+          </div>
+        </el-form-item>
+        <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
+        <el-form-item style="width:100%;">
+          <el-button
             :loading="loading"
             size="medium"
             style="width:100%;"
             type="primary"
             @click.native.prevent="handleLogin"
-        >
-          <span v-if="!loading">登 录</span>
-          <span v-else>登 录 中...</span>
-        </el-button>
-        <div v-if="register" style="float: right;">
-          <router-link :to="'/register'" class="link-type">立即注册</router-link>
-        </div>
-        <el-button type="text" style="float: right;margin-top: 10px;margin-bottom: -5px" @click="showVersion">版本变更记录</el-button>
-      </el-form-item>
-    </el-form>
+          >
+            <span v-if="!loading">登 录</span>
+            <span v-else>登 录 中...</span>
+          </el-button>
+          <div v-if="register" style="float: right;">
+            <router-link :to="'/register'" class="link-type">立即注册</router-link>
+          </div>
+          <el-button type="text" style="float: right;margin-top: 10px;margin-bottom: -5px" @click="showVersion">版本变更记录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
     <!--  底部  -->
     <div class="el-login-footer">
       <span>Copyright © 2018-2023 ruoyi.vip All Rights Reserved.  <a href="https://beian.miit.gov.cn/">鲁ICP备2024105874号</a> </span>
@@ -68,36 +78,36 @@
 </template>
 
 <script>
-import {getCodeImg} from "@/api/login";
-import Cookies from "js-cookie";
-import {decrypt, encrypt} from '@/utils/jsencrypt'
+import { getCodeImg } from '@/api/login'
+import Cookies from 'js-cookie'
+import { decrypt, encrypt } from '@/utils/jsencrypt'
 import versionChange from '@/views/version/index.vue'
 
 export default {
-  name: "Login",
+  name: 'Login',
   components: {
     versionChange
   },
   data() {
     return {
-
+      loginType: 1,
       versionchangeShow: false,
-      codeUrl: "",
+      codeUrl: '',
       loginForm: {
-        username: "18616771546",
-        password: "admin",
+        username: '18616771546',
+        password: 'admin',
         rememberMe: false,
-        code: "",
-        uuid: ""
+        code: '',
+        uuid: ''
       },
       loginRules: {
         username: [
-          {required: true, trigger: "blur", message: "请输入您的账号"}
+          { required: true, trigger: 'blur', message: '请输入您的账号' }
         ],
         password: [
-          {required: true, trigger: "blur", message: "请输入您的密码"}
+          { required: true, trigger: 'blur', message: '请输入您的密码' }
         ],
-        code: [{required: true, trigger: "change", message: "请输入验证码"}]
+        code: [{ required: true, trigger: 'change', message: '请输入验证码' }]
       },
       loading: false,
       // 验证码开关
@@ -105,71 +115,71 @@ export default {
       // 注册开关
       register: false,
       redirect: undefined
-    };
+    }
   },
   watch: {
     $route: {
-      handler: function (route) {
-        this.redirect = route.query && route.query.redirect;
+      handler: function(route) {
+        this.redirect = route.query && route.query.redirect
       },
       immediate: true
     }
   },
   created() {
-    this.getCode();
-    this.getCookie();
-    const h = this.$createElement;
+    this.getCode()
+    this.getCookie()
+    const h = this.$createElement
   },
   methods: {
     getCode() {
       getCodeImg().then(res => {
-        this.captchaEnabled = res.captchaEnabled === undefined ? true : res.captchaEnabled;
+        this.captchaEnabled = res.captchaEnabled === undefined ? true : res.captchaEnabled
         if (this.captchaEnabled) {
-          this.codeUrl = "data:image/gif;base64," + res.img;
-          this.loginForm.uuid = res.uuid;
+          this.codeUrl = 'data:image/gif;base64,' + res.img
+          this.loginForm.uuid = res.uuid
         }
-      });
+      })
     },
     getCookie() {
-      const username = Cookies.get("username");
-      const password = Cookies.get("password");
+      const username = Cookies.get('username')
+      const password = Cookies.get('password')
       const rememberMe = Cookies.get('rememberMe')
       this.loginForm = {
         username: username === undefined ? this.loginForm.username : username,
         password: password === undefined ? this.loginForm.password : decrypt(password),
         rememberMe: rememberMe === undefined ? false : Boolean(rememberMe)
-      };
+      }
     },
     showVersion() {
-      this.versionchangeShow = true;
+      this.versionchangeShow = true
     },
     handleLogin() {
       this.$refs.loginForm.validate(valid => {
         if (valid) {
-          this.loading = true;
+          this.loading = true
           if (this.loginForm.rememberMe) {
-            Cookies.set("username", this.loginForm.username, {expires: 30});
-            Cookies.set("password", encrypt(this.loginForm.password), {expires: 30});
-            Cookies.set('rememberMe', this.loginForm.rememberMe, {expires: 30});
+            Cookies.set('username', this.loginForm.username, { expires: 30 })
+            Cookies.set('password', encrypt(this.loginForm.password), { expires: 30 })
+            Cookies.set('rememberMe', this.loginForm.rememberMe, { expires: 30 })
           } else {
-            Cookies.remove("username");
-            Cookies.remove("password");
-            Cookies.remove('rememberMe');
+            Cookies.remove('username')
+            Cookies.remove('password')
+            Cookies.remove('rememberMe')
           }
-          this.$store.dispatch("Login", this.loginForm).then(() => {
-            this.$router.push({path: this.redirect || "/"}).catch(() => {
-            });
+          this.$store.dispatch('Login', this.loginForm).then(() => {
+            this.$router.push({ path: this.redirect || '/' }).catch(() => {
+            })
           }).catch(() => {
-            this.loading = false;
+            this.loading = false
             if (this.captchaEnabled) {
-              this.getCode();
+              this.getCode()
             }
-          });
+          })
         }
-      });
+      })
     }
   }
-};
+}
 </script>
 
 <style lang="scss" rel="stylesheet/scss">
@@ -193,6 +203,7 @@ export default {
   background: #ffffff;
   width: 400px;
   padding: 25px 25px 5px 25px;
+  height: 350px;
 
   .el-input {
     height: 38px;
@@ -241,5 +252,35 @@ export default {
 
 .login-code-img {
   height: 38px;
+}
+
+.useOtherLogin {
+  float: right;
+  margin-right: -25px;
+  margin-top: -25px;
+  width: 60px;
+  background-size: contain;
+  top: -20px;
+  height: 60px;
+  background-image: url("../assets/images/qr-login.png");
+}
+
+.usePwdLogin {
+  float: right;
+  margin-right: -25px;
+  margin-top: -25px;
+  width: 60px;
+  background-size: contain;
+  top: -20px;
+  height: 60px;
+  background-image: url("../assets/images/pwd-login.png");
+}
+
+.dingLoginDiv {
+  border-radius: 6px;
+  width: 400px;
+  height: 350px;
+  background: #ffffff;
+  padding: 25px;
 }
 </style>
